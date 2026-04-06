31f6f6d30a02870fc508e5c005ee23f78ae56534
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:04:53 +0700

    mengubah array

--- Array.cpp
+++ Array.cpp
@@ -5,5 +5,7 @@ int main(){
     int aku [5] = {10,20,30,40,50}; //deklarasi dengan inisialisasi
     int kamu[5]; //deklarasi tanpa inisialisasi
 
-    cout<<"Data pada Index 1 = "<<aku[1]<<endl;
+    cout<<"Data pada Index 1 = "<<aku[1]<<endl; //tampilkan data
+
+    aku[1] = 200;
 }
