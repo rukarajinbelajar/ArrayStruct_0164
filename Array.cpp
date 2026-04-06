1c3991a827e3cecc52a4b8fd68b291f632261d5c
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:03:29 +0700

    mengakses dan mengubah array

--- Array.cpp
+++ Array.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
     int aku [5] = {10,20,30,40,50}; //deklarasi dengan inisialisasi
     int kamu[5]; //deklarasi tanpa inisialisasi
-    
+
+    cout<<"Data pada Index 1 = "<<aku[1]<<endl;
 }
