3792f990b8c5c6ab4392c9dda086d37165040efc
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:56:28 +0700

    merapikan

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -31,11 +31,11 @@ int main(){
 
     for (int i = 0; i <= 2; i++)
     {
-        cout<<" Data Penduduk : " <<endl;
-        cout<<" Nama : "<<mhs[i].nama <<endl;
-        cout<<" Desa : "<<mhs[i].alamat.desa <<endl;
-        cout<<" Kota : "<<mhs[i].alamat.kota <<endl;
-        cout<<" Umur : "<<mhs[i].umur <<endl;
+        cout<<"Data Penduduk : " <<endl;
+        cout<<"Nama : "<<mhs[i].nama <<endl;
+        cout<<"Desa : "<<mhs[i].alamat.desa <<endl;
+        cout<<"Kota : "<<mhs[i].alamat.kota <<endl;
+        cout<<"Umur : "<<mhs[i].umur <<endl;
         cout<<endl;
     }
 }
