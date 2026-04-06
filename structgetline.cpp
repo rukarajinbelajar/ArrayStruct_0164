50e5c6301c2065d7c78086e66872f178d3100ba2
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:51:16 +0700

    membenarkan

---
+++ structgetline.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main(){
+    Orang mhs;
+    cout <<"Nama : ";
+    getline(cin, mhs.nama);
+    cout <<"Alamat Desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout <<"Alamat Kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout <<"Umur : ";
+    cin>>mhs.umur;
+    cout<<endl;
+
+    //tampilkan
+    cout<<" Nama : "<<mhs.nama <<endl;
+    cout<<" Desa : "<<mhs.alamat.desa <<endl;
+    cout<<" Kota : "<<mhs.alamat.kota <<endl;
+    cout<<" Umur : "<<mhs.umur <<endl;
+}
