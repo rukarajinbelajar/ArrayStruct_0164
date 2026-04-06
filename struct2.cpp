49ce0fce336a5e961528921d3f22bc704f8e25ae
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:31:11 +0700

    menambahkan struct desa dan kota

---
+++ struct2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+struct Orang
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    string umur;
+};
+
+int main(){
+    Orang mhs;
+    cout <<"Nama : ";
+    cin>>mhs.nama;
+    cout <<"Alamat : ";
+    cin>>mhs.alamat;
+    cout <<"Umur : ";
+    cin>>mhs.umur;
+    cout<<endl;
+
+    //tampilkan
+    cout<<" Nama : "<<mhs.nama <<endl;
+    cout<<" Alamat : "<<mhs.alamat <<endl;
+    cout<<" Umur : "<<mhs.umur <<endl;
+}
