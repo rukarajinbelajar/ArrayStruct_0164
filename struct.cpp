8e4931e863d8e3e61476ffc6328f49879afa0250
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:26:47 +0700

    membuat variabel, dan membuat variabel dari struct

--- struct.cpp
+++ struct.cpp
@@ -6,4 +6,15 @@ struct Orang
     string nama;
     string alamat;
     string umur;
+};
+
+int main(){
+    Orang mhs;
+    cout <<"Nama : ";
+    cin>>mhs.nama;
+    cout <<"Alamat : ";
+    cin>>mhs.alamat;
+    cout <<"Umur : ";
+    cin>>mhs.umur;
+    cout<<endl;
 }
