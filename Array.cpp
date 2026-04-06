4588e40563fcc1f60b448bdf65cd5a355a3b12c5
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-06 15:02:17 +0700

    array ddengan isi

--- Array.cpp
+++ Array.cpp
@@ -1,2 +1,8 @@
 #include<iostream>
 using namespace std;
+
+int main(){
+    int aku [5] = {10,20,30,40,50}; //deklarasi dengan inisialisasi
+    int kamu[5]; //deklarasi tanpa inisialisasi
+    
+}
